c4c22688eff75fe515948f7cdf6bb5b9047abd38
Revlen <uverogre@gmail.com>
2021-07-22 17:42:47 +0200

    Make user 'registration'

--- app/Http/Controllers/BotCommands/StartCommand.php
+++ app/Http/Controllers/BotCommands/StartCommand.php
@@ -4,6 +4,7 @@
 namespace App\Http\Controllers\BotCommands;
 
 
+use App\Models\User;
 use Telegram\Bot\Commands\Command;
 
 class StartCommand extends Command
@@ -23,8 +24,20 @@ class StartCommand extends Command
      */
     public function handle()
     {
+        $chatId = $this->getUpdate()->getMessage()->getChat()->getId();
+
+        $user = User::where('chat_id', $chatId)->first();
+
+        if(!$user){
+            User::create([
+                'chat_id' => $chatId,
+                'username' => $this->getUpdate()->getMessage()->getChat()->getUsername(),
+                'name' => $this->getUpdate()->getMessage()->getChat()->getFirstName(),
+            ]);
+        }
+
         $this->replyWithMessage([
-            'text' => config('bot.welcome_text', 'Welcome' . $this->update->getChat()->username)
+            'text' => config('bot.welcome_text', 'Welcome ' . $this->getUpdate()->getMessage()->getChat()->getUsername())
         ]);
     }
 }

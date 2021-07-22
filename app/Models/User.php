91a3f560ee7904fe27754c05b0390d9f21f34fda
Revlen <uverogre@gmail.com>
2021-07-22 17:05:11 +0200

    Create message model, update relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,9 +17,9 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
+        'chat_id',
+        'username',
         'name',
-        'email',
-        'password',
     ];
 
     /**
@@ -40,4 +40,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function messages()
+    {
+        $this->hasMany(Message::class);
+    }
 }
